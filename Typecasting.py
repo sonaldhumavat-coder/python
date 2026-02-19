496de9cdaabefae4e2ae1ee862eed542138da814
sonaldhumavat-coder <sonal.dhumavat@gmail.com>
2026-02-19 14:40:16 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,16 @@
+a="50"
+print("type of a  is",type(a))
+b=200
+print("type of b  is",type(b))
+c=True
+print("type of c is",type(c))
+d=4.27367273892
+print("type of dis",type(d))
+newa=int(a)
+print(type(newa))
+newb=str(b)
+print(type(newb))
+newc=bool(c)
+print(type(newc))
+newd=float(d)
+print(type(newd))
