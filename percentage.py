8df42158644273ca8e538858b8f9f500979b3767
sonaldhumavat-coder <sonal.dhumavat@gmail.com>
2026-02-20 14:54:14 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,8 @@
+english=float(input("Enter your marks of english out of 20"))
+math=float(input("Enter your marks of math out of 20"))
+science=float(input("Enter your marks of science out of 20"))
+hindi=float(input("Enter your marks of hindi out of 20"))
+gujarati=float(input("Enter your marks of gujarati out of 20"))
+sum=english+math+science+hindi+gujarati
+percentage=(sum/100)*100
+print("Your percentage is",percentage,"%")
