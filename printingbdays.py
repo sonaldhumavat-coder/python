2bd69a6970ffb8085e607357a0960aa7ac92c898
sonaldhumavat-coder <sonal.dhumavat@gmail.com>
2026-02-20 13:53:02 +0530

    Add files via upload

---
+++ printingbdays.py
@@ -0,0 +1,18 @@
+
+
+var1 = "14/11/2014"
+name1 = "Hrishikesh"
+
+var2 = "18/11/2011"
+name2 = "Pritha"
+
+var3 = "17/12/1984"
+name3 = "Sonal"
+
+var4 = "28/1/1983"
+name4 = "Nirav"
+
+print(var1, "birthday of", name1)
+print(var2, "birthday of", name2)
+print(var3, "birthday of", name3)
+print(var4, "birthday of", name4)
