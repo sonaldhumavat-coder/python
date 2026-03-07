ef2433f2d4ea1d14fdbf4c185c305338505e5368
sonaldhumavat-coder <sonal.dhumavat@gmail.com>
2026-03-07 14:15:23 +0530

    Add files via upload

---
+++ storingbdays.py
@@ -0,0 +1,11 @@
+birthday1 = "12 January"
+birthday2 = "5 March"
+birthday3 = "18 April"
+birthday4 = "22 August"
+birthday5 = "30 December"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
